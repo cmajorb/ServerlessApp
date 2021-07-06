e6acf60156c6897227f844773b3f1e1bb01ce5ea
cmajorb <cmajorb@gmail.com>
2021-07-05 17:30:05 -0700

    added config file

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_TUpZqJTSL',
+        userPoolClientId: '55der2ess97ai9j20g9de8j7l8',
+        region: 'us-east-1'
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
