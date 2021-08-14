6d8e922d2256e1750ce1dd03a5f1bbb31527813b
cmajorb <cmajorb@gmail.com>
2021-08-14 14:39:05 -0700

    Added percentages and changing scrolling

--- django/bbs/bbsapi/serializers.py
+++ django/bbs/bbsapi/serializers.py
@@ -3,8 +3,6 @@ from rest_framework import serializers
 from .models import Contract, Tenant, Owner, Property
 
 
-
-
 class OwnerSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Owner
@@ -16,19 +14,18 @@ class PropertySerializer(serializers.ModelSerializer):
         fields = ('id','name','sitenumber','modified','added')
 
 class ContractSerializer(serializers.ModelSerializer):
-    #tenant = TenantSerializer(many=False,read_only=True)
-    #tenantname = serializers.SlugRelatedField(many=False, read_only=True,slug_field='tenantname')
     tenant = serializers.CharField(read_only=True,source='tenantid.name')
     owner = serializers.CharField(read_only=True,source='ownerid.name')
     propertyname = serializers.CharField(read_only=True,source='propertyid.name')
-    #tenant = serializers.StringRelatedField(many=False)
+    managementfee_p = serializers.SerializerMethodField()
     class Meta:
         model = Contract
-        fields = ('id','contractid', 'ownerid','tenantid','tenant','propertyid','propertyname','owner','expdate','increasedate','increasepercentage','baserent','salestax','utilities','managementfee','modified','added')
+        fields = ('id','contractid', 'ownerid','tenantid','tenant','propertyid','propertyname','owner','expdate','increasedate','increasepercentage','baserent','salestax','utilities','managementfee','managementfee_p','modified','added')
+    def get_managementfee_p(self,obj):
+        return "{:.0%}".format(obj.managementfee)
 
 class TenantSerializer(serializers.ModelSerializer):
     contracts = serializers.StringRelatedField(many=True)
-    #contracts = ContractSerializer(many=True)
     class Meta:
         model = Tenant
         fields = ('id','name','contracts','modified','added')
